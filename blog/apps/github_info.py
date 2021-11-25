95ec3e337cdf712e10e124c7e234b680c5bacc24
kamuridesu <myk.gata14@gmail.com>
2021-11-25 16:39:15 -0300

    give up on search, logos added

--- blog/apps/github_info.py
+++ blog/apps/github_info.py
@@ -1,4 +1,8 @@
 import requests
+import json
+import random
+import string
+import time
 
 
 class UserNotSupportedError(Exception):
@@ -6,8 +10,19 @@ class UserNotSupportedError(Exception):
         super().__init__(*args, "User cannot be empty or is invalid!")
 
 
+def randomCode():
+    rd_string = "".join([random.choice(string.ascii_letters + string.digits) for _ in range(21)])
+    return rd_string
+
+
 class GithubInfo:
     def __init__(self, username: str) -> None:
+        if not username or username == "":
+            raise UserNotSupportedError
+        self.username = username
+        with open("config.auth.json", "r", encoding="utf-8") as file:
+            self.api_secrets = json.loads(file.read())
+
         self.api_base_url = f"https://api.github.com/users/{username}/repos"
 
     def processResponse(self, response: list) -> list:
@@ -22,14 +37,35 @@ class GithubInfo:
                 "description": item['description'],
                 })
         return new_response
+
+    def hasLimitLeft(self) -> dict:
+        has_limit = False
+        header = {
+            "Authorization": f"token {self.api_secrets['kamutoken']}"
+            }
+        response = requests.get(f"https://api.github.com/rate_limit", headers=header)
+        if response.status_code == 200:
+            response = response.json()
+            if response['resources']['core']['remaining'] > 0:
+                has_limit = True
+            reset_time = response['resources']['core']['reset'] - int(time.time()) 
+            return {"reset": reset_time, "has_limit": has_limit}
+        return {"has_limit": False, "reset": -1}
     
     def makeRequest(self) -> list:
-        response = requests.get(self.api_base_url)
+        header = {
+            "Authorization": f"token {self.api_secrets['kamutoken']}"
+            }
+        has_limit = self.hasLimitLeft()
+        if has_limit['has_limit']:
+            response = requests.get(self.api_base_url, headers=header)
+        print("Rate limit resets in", has_limit['reset'])
         if response.status_code == 200:
             return self.processResponse(response.json())
         print(response.content)
+        raise UserNotSupportedError
 
 
 if __name__ == "__main__":
-    github = GithubInfo("")
-    github.makeRequest()
+    github = GithubInfo("kamuridesu")
+    (github.makeRequest())
--- blog/main.py
+++ blog/main.py
@@ -1,33 +1,46 @@
 from flask import Flask, request, redirect, render_template
 import os
 
-from apps.github_info import GithubInfo
+from apps.github_info import GithubInfo, UserNotSupportedError
 from apps.functions import findRepos
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
 
 
+@app.route("/callback")
+def callback():
+    return redirect("/")
+
+
+@app.route("/error")
+def error_handler():
+    req = request.args.get("error")
+    err_code = request.args.get("code")
+    if not req or not err_code:
+        return redirect("/")
+    return render_template("generic_error.html", error=req), err_code
+
+
 @app.route("/projects")
 def projects():
-    g_info = GithubInfo("kamuridesu").makeRequest()
-    return render_template("projects.html", infos=g_info)
+    try:
+        g_info = GithubInfo("kamuridesu").makeRequest()
+        return render_template("projects.html", infos=g_info)
+    except UserNotSupportedError:
+        return redirect("/error?error=github api error&code=500")
+    return redirect("/")
 
-
-@app.route("/search")
-def search():
-    req = request.args.get("content")
-    response = []
-    if req:
-        response = findRepos(req, GithubInfo("kamuridesu").makeRequest())
-        print(response)
-    return render_template("search.html", items=response)
 
 
 @app.route("/")
 def index():
-    print("a")
-    return render_template("index.html")
+    logos = [
+        {"image": "/static/images/github_logo.png", "url": "https://github.com/kamuridesu", "alt": "github logo"},
+        {"image": "/static/images/twitter-logo.png", "url": "https://twitter.com/kamuri_chan", "alt": "twitter logo"},
+        {"image": "/static/images/telegram-logo.png", "url": "https://t.me/kamuridesu", "alt": "telegram logo"}
+    ]
+    return render_template("index.html", logos=logos)
 
 
 if __name__ == "__main__":
